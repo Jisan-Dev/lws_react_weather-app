f771fef8c217949f6cdadae4ef8fe1bc694e63da
Jisun696 <istiakkashem35@gmail.com>
2025-09-08 03:44:01 +0600

    handle the favorite list modal display on condition

--- src/components/header/Favorites.jsx
+++ src/components/header/Favorites.jsx
@@ -1,24 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import HeartIcon from "../../assets/heart.svg";
 import { FavoritesContext } from "../../contexts";
 
 export default function Favorites() {
   const { favorites } = useContext(FavoritesContext);
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
-      <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
-        <img src="./assets/heart.svg" alt="" />
+      <div
+        onClick={() => setShowModal(!showModal)}
+        className="p-2 text-white hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+        <img src={HeartIcon} alt="Heart Icon" />
         <span>Favorite Locations</span>
       </div>
 
       {/* <!-- Modal --> */}
-      <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
+
+      <div
+        className={`${
+          showModal ? "opacity-100 visible" : "opacity-0 invisible"
+        } transition-all duration-300 max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg`}>
         <h3 className="text-lg font-bold px-4">Favorite Locations</h3>
         <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-          {favorites.map((fav) => (
-            <li key={fav.location} className="hover:bg-gray-200">
-              {fav.location}
-            </li>
-          ))}
+          {favorites.length > 0 ? (
+            favorites.map((fav) => (
+              <li key={fav.location} className="hover:bg-gray-200">
+                {fav.location}
+              </li>
+            ))
+          ) : (
+            <li className="text-sm text-center text-gray-500">No favorite locations added.</li>
+          )}
         </ul>
       </div>
     </>
